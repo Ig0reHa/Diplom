19dbba18bd62d71211b486af5e73b887a3e7fe75
Ig0reHa <36538511+Ig0reHa@users.noreply.github.com>
2018-09-08 16:53:31 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -253,12 +253,26 @@ window.addEventListener('DOMContentLoaded', () => {
 	// Гамбургер-меню
 
 	let burgerBtn = document.getElementsByClassName('burger')[0],
-		burgerMenu = document.getElementsByClassName('burger-menu')[0];
+		burgerMenu = document.getElementsByClassName('burger-menu')[0],
+		body = document.getElementsByTagName('body')[0];
 
 	if(window.matchMedia('(max-width: 768px)').matches) {
+	  let clicked = 2;
 	  burgerBtn.onclick = () => {
-	  	burgerMenu.toggle();
+	  	if (clicked % 2 == 0) {
+	  		burgerMenu.style.display = 'block';
+	  		clicked++;
+	  	} else if (clicked % 2 != 0) {
+	  		burgerMenu.style.display = 'none';
+	  		clicked++;
+	  	}
 	  }
+	}
+
+	body.onresize = () => {
+		if(window.matchMedia('(min-width: 768px)').matches) {
+		  burgerMenu.style.display = 'none';
+		}
 	}
 
 });
