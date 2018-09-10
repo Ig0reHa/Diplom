1395c28202ec3651e82d0dab0207d7285e0c822f
Ig0reHa <36538511+Ig0reHa@users.noreply.github.com>
2018-09-10 19:09:32 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -271,6 +271,8 @@ window.addEventListener('DOMContentLoaded', () => {
 	});
 
 
+
+
 	// Гамбургер-меню
 
 	let burgerBtn = document.getElementsByClassName('burger')[0],
@@ -295,5 +297,82 @@ window.addEventListener('DOMContentLoaded', () => {
 		  burgerMenu.style.display = 'none';
 		}
 	}
+
+
+
+
+	// Нижний слайдер
+
+
+	function Carousel(setting) {
+		if(document.querySelector(setting.wrap) === null) {
+			console.error(`Carousel not fount selector ${setting.wrap}`);
+			return;
+		}
+
+		let privates = {};
+
+
+		this.prev_slide = () => {
+			--privates.opt.position;
+
+			if(privates.opt.position < 0) {
+				privates.sel.wrap.classList.add('s-notransition');
+				privates.opt.position = privates.opt.max_position - 1;
+			}
+
+			privates.sel.wrap.style["transform"] = `translateX(-${privates.opt.position}00%)`;
+		};
+
+
+
+		this.next_slide = () => {
+			++privates.opt.position;
+
+			if(privates.opt.position >= privates.opt.max_position) {
+				privates.opt.position = 0;
+			}
+
+			privates.sel.wrap.style["transform"] = `translateX(-${privates.opt.position}00%)`;
+		};
+
+		setInterval(this.next_slide, 10000);
+
+
+		privates.setting = setting;
+
+		privates.sel = {
+			"main": document.querySelector(privates.setting.main),
+			"wrap": document.querySelector(privates.setting.wrap),
+			"children": document.querySelector(privates.setting.wrap).children,
+			"prev": document.querySelector(privates.setting.prev),
+			"next": document.querySelector(privates.setting.next)
+		};
+
+		privates.opt = {
+			"position": 0,
+			"max_position": document.querySelector(privates.setting.wrap).children.length
+		};
+
+		if(privates.sel.prev !== null) {
+			privates.sel.prev.addEventListener('click', () => {
+				this.prev_slide();
+			});
+		}
+
+		if(privates.sel.next !== null) {
+			privates.sel.next.addEventListener('click', () => {
+				this.next_slide();
+			});
+		}
+	}
+
+	let a = new Carousel({
+		"main": ".feedback-slider",
+		"wrap": ".feedback-wrapper",
+		"prev": ".main-prev-btn",
+		"next": ".main-next-btn"
+	});
+
 
 });
